5cd2cac275e1bbc98f69444c0e81c3a158a41cda
lhjeong60 <lhjeong60@naver.com>
2023-03-08 23:09:30 +0900

    fix: modify lib/plugins/reservedMeta.js

--- modify.js
+++ modify.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 let index = fs.readFileSync('lib/index.js', 'utf8');
-
+let reservedMeta = fs.readFileSync('lib/plugins/reservedMeta.js', 'utf8');
 index = index.replace(`require("react-markdown")`, `( async () => await import("react-markdown"))()`);
 index = index.replace(`require("remark-gfm")`, `( async () => await import("remark-gfm"))()`);
 index = index.replace(`require("rehype-raw")`, `( async () => await import("rehype-raw"))()`);
@@ -15,6 +15,9 @@ index = index.replace(`require("rehype-ignore")`, `( async () => await import("r
 index = index.replace(`require("rehype-prism-plus")`, `( async () => await import("rehype-prism-plus"))()`);
 index = index.replace(`require("rehype-rewrite")`, `( async () => await import("rehype-rewrite"))()`);
 
+reservedMeta = reservedMeta.replace(`require("unist-util-visit")`, `(async () => await import("unist-util-visit"))()`);
+
 fs.writeFileSync('lib/index.js', index, 'utf8');
+fs.writeFileSync('lib/plugins/reservedMeta.js', reservedMeta, 'utf8');
 
 console.log('Modify Done');
